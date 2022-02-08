968776faafee66c48cd9435d1da93802e6bdd8e0
Tredicibrando <nicolosigiov@gmail.com>
2022-02-08 15:12:36 +0100

    impostazione css e html e creazione ciclo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log('ciao bello')
+
+
+for (let i = 1; i <=100 ; i++) {
+    console.log(i)
+
+    if(i % 3 === 0){
+        console.log('fizz', i)
+    } else if (i % 5 === 0){
+        console.log ('buzz' , i)
+    }
+}
