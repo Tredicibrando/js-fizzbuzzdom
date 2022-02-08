f102f57f462d2be697e65184dd5add3465fc7109
Tredicibrando <nicolosigiov@gmail.com>
2022-02-08 15:33:08 +0100

    esercizio completo

--- js/app.js
+++ js/app.js
@@ -13,14 +13,17 @@ for (let i = 1; i <=100 ; i++) {
     if(i % 3 === 0 && i % 5 === 0){
         console.log('fizzbuzz', i)
         div.append('fizzbuzz')
+        div.classList.add("box--fizzbuzz")
         
     }
     else if(i % 3 === 0){
         console.log('fizz', i)
         div.append('fizz')
+        div.classList.add("box--fizz")
     } else if (i % 5 === 0){
         console.log ('buzz', i)
         div.append('buzz')
+        div.classList.add("box--buzz")
     } else{
         div.append(i)
     }
