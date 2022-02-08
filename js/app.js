0887885d367a1aa0dcb493999bd682bc362e66a1
Tredicibrando <nicolosigiov@gmail.com>
2022-02-08 15:30:40 +0100

    Finalizzazione css

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
 console.log('ciao bello')
 
 
+const container = document.getElementById('main_container')
+
+
 for (let i = 1; i <=100 ; i++) {
     console.log(i)
+    const div = document.createElement('div')
+    div.classList.add("box")
 
-    if(i % 3 === 0){
-        console.log('fizz', i)
-    } else if (i % 5 === 0){
-        console.log ('buzz' , i)
+
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log('fizzbuzz', i)
+        div.append('fizzbuzz')
+        
     }
+    else if(i % 3 === 0){
+        console.log('fizz', i)
+        div.append('fizz')
+    } else if (i % 5 === 0){
+        console.log ('buzz', i)
+        div.append('buzz')
+    } else{
+        div.append(i)
+    }
+
+    container.append(div)
 }
